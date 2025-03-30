4ade2ecaf19e7e92b8e5681b4e072029a5b75662
deepanshgandhi <34491946+deepanshgandhi@users.noreply.github.com>
2025-03-30 15:11:46 -0400

    Time: 43 ms (58.56%) | Memory: 52.9 MB (53.93%) - LeetSync

---
+++ 2734-array-prototype-last/array-prototype-last.js
@@ -0,0 +1,14 @@
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    len = this.length
+    if(len>0)
+        return this[len-1]
+    return -1
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
