5c100160b96652390f883c5fa4cc417a09b6debb
deepanshgandhi <34491946+deepanshgandhi@users.noreply.github.com>
2025-03-31 17:26:38 -0400

    Time: 34 ms (93.15%) | Memory: 54.6 MB (5.28%) - LeetSync

--- 2732-counter/counter.js
+++ 2732-counter/counter.js
@@ -2,12 +2,12 @@
  * @param {number} n
  * @return {Function} counter
  */
-var createCounter = function(n) {
-    
-    return function() {
-        return n++;
-    };
-};
+function createCounter(n){
+    count = n;
+    return function(){
+        return count++;
+    }
+}
 
 /** 
  * const counter = createCounter(10)
